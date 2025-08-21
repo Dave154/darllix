c913dac5a6bc001642ebeff14d131697228aa38f
David Okpe <okpedavid0@gmail.com>
2025-08-21 01:05:51 +0100

    update

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -24,7 +24,7 @@ export default function DashboardPage() {
           className="bg-black text-white p-4 rounded-md flex justify-between items-center"
         >
           <span>
-            Extend your trial for $1/month for 3 months on select plans.
+            Extend your trial for ₦5000/month for 3 months on select plans.
           </span>
           <Button className="bg-white text-black hover:bg-gray-200">
             Select a plan
