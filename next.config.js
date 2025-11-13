fac5a4921a0ca1b3210ae006dc4138e19a22e926
David Okpe <okpedavid0@gmail.com>
2025-11-13 15:40:30 +0100

    updaa\te

--- next.config.js
+++ next.config.js
@@ -1,84 +1,48 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       { protocol: "https", hostname: "darllix.shop" },
-//       { protocol: "https", hostname: "*.darllix.shop" },
-//        {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//       },
-//     ],
-//   },
-//   async rewrites() {
-//     return [
-//       // Root domain
-//       {
-//         source: "/",
-//         destination: "/storefront",
-//       },
-//       {
-//         source: "/:slug*",
-//         destination: "/storefront/:slug*",
-//       },
-
-//         {
-//       source: "/product/:id",
-//       destination: "/storefront/product/[id]",
-//     },
-//       // Subdomains
-//       {
-//         source: "/",
-//         has: [
-//           { type: "host", value: ":subdomain.darllix.shop" },
-//         ],
-//         destination: "/storefront",
-//       },
-//       {
-//         source: "/:slug*",
-//         has: [
-//           { type: "host", value: ":subdomain.darllix.shop" },
-//         ],
-//         destination: "/storefront/:slug*?store=:subdomain",
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "darllix.shop" },
-      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "*.darllix.shop" },
+       {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
   async rewrites() {
     return [
-      // === Subdomain storefronts only ===
-      // Example: store.darllix.shop/   -> /storefront?store=store
+      // Root domain
       {
         source: "/",
-        has: [{ type: "host", value: ":subdomain.darllix.shop" }],
         destination: "/storefront",
       },
-
       {
         source: "/:slug*",
-        has: [{ type: "host", value: ":subdomain.darllix.shop" }],
-        destination: "/storefront/:slug",
+        destination: "/storefront/:slug*",
       },
 
+        {
+      source: "/product/:id",
+      destination: "/storefront/product/[id]",
+    },
+      // Subdomains
       {
-        source: "/product/:id",
-        destination: "/storefront/product/:id",
+        source: "/",
+        has: [
+          { type: "host", value: ":subdomain.darllix.shop" },
+        ],
+        destination: "/storefront",
       },
-
-    ]
+      {
+        source: "/:slug*",
+        has: [
+          { type: "host", value: ":subdomain.darllix.shop" },
+        ],
+        destination: "/storefront/:slug*?store=:subdomain",
+      },
+    ];
   },
-}
+};
 
 module.exports = nextConfig;
