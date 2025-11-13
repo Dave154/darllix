6d66faa847995ae49e50967ed451c3138a5e5c13
David Okpe <okpedavid0@gmail.com>
2025-11-13 17:36:16 +0100

    updaa\te

--- next.config.js
+++ next.config.js
@@ -12,34 +12,51 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      // Root domain
       {
         source: "/",
         destination: "/storefront",
       },
-      {
-        source: "/checkout",
-        destination: "/storefront/checkout/",
-      },
-       {
-        source: "/checkout/payment",
-        destination: "/storefront/checkout/payment",
-      },
-      {
-        source: "/checkout/shipping",
-        destination: "/storefront/checkout/shipping",
-      },
-      {
-        source: "/cart",
-        destination: "/storefront/cart/",
-      },
-      {
-        source: "/payment-success",
-        destination: "/storefront/payment-success/",
-      },
-       {
-      source: "/product",
-      destination: "/storefront/product/",
+      // {
+      //   source: "/:slug",
+      //   destination: "/storefront/:slug*",
+      // },
+      // {
+      //   source: "/checkout",
+      //   destination: "/storefront/checkout/",
+      // },
+      //  {
+      //   source: "/checkout/payment",
+      //   destination: "/storefront/checkout/payment",
+      // },
+      // {
+      //   source: "/checkout/shipping",
+      //   destination: "/storefront/checkout/shipping",
+      // },
+      // {
+      //   source: "/cart",
+      //   destination: "/storefront/cart/",
+      // },
+      // {
+      //   source: "/payment-success",
+      //   destination: "/storefront/payment-success/",
+      // },
+      //  {
+      // source: "/product",
+      // destination: "/storefront/product/",
+    // },
+    {
+      source: "/:slug*",
+      has: [
+        { type: "host", value: "(?<subdomain>.*).darllix.shop" },
+      ],
+      destination: "/storefront/:slug*?store=:subdomain",
+    },
+    {
+      source: "/",
+      has: [
+        { type: "host", value: "(?<subdomain>.*).darllix.shop" },
+      ],
+      destination: "/storefront?store=:subdomain",
     },
       {
       source: "/product/:id",
