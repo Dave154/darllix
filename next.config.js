d4b52a99da171b42a9e72f45a00214c59c1c50d6
David Okpe <okpedavid0@gmail.com>
2025-11-13 17:14:03 +0100

    updaa\te

--- next.config.js
+++ next.config.js
@@ -10,39 +10,58 @@ const nextConfig = {
       },
     ],
   },
-  // async rewrites() {
-  //   return [
-  //     // Root domain
-  //     {
-  //       source: "/",
-  //       destination: "/storefront",
-  //     },
-  //     {
-  //       source: "/:slug*",
-  //       has: [{ type: "host", value: ":subdomain.darllix.shop" }],
-  //       destination: "/storefront/:slug*",
-  //     },
-  //       {
-  //     source: "/product/:id",
-  //     destination: "/storefront/product/[id]",
-  //   },
-  //     // Subdomains
-  //     {
-  //       source: "/",
-  //       has: [
-  //         { type: "host", value: ":subdomain.darllix.shop" },
-  //       ],
-  //       destination: "/storefront",
-  //     },
-  //     {
-  //       source: "/:slug*",
-  //       has: [
-  //         { type: "host", value: ":subdomain.darllix.shop" },
-  //       ],
-  //       destination: "/storefront/:slug*?store=:subdomain",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      // Root domain
+      {
+        source: "/",
+        destination: "/storefront",
+      },
+      {
+        source: "/checkout",
+        destination: "/storefront/checkout/",
+      },
+       {
+        source: "/checkout/payment",
+        destination: "/storefront/checkout/payment",
+      },
+      {
+        source: "/checkout/shipping",
+        destination: "/storefront/checkout/shipping",
+      },
+      {
+        source: "/cart",
+        destination: "/storefront/cart/",
+      },
+      {
+        source: "/payment-success",
+        destination: "/storefront/payment-success/",
+      },
+       {
+      source: "/product",
+      destination: "/storefront/product/",
+    },
+      {
+      source: "/product/:id",
+      destination: "/storefront/product/[id]",
+    },
+      // Subdomains
+      {
+        source: "/",
+        has: [
+          { type: "host", value: ":subdomain.darllix.shop" },
+        ],
+        destination: "/storefront",
+      },
+      {
+        source: "/:slug*",
+        has: [
+          { type: "host", value: ":subdomain.darllix.shop" },
+        ],
+        destination: "/storefront/:slug*?store=:subdomain",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
